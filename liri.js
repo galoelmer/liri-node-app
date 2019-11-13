8c5d5de9f272055d3c249072fcb9ba02f88f5654
Elmer C. Galo <elmer.galo@outlook.com>
2019-11-13 16:20:38 -0500

    Add API request to OMDB Api to obtain movie information

--- liri.js
+++ liri.js
@@ -11,8 +11,8 @@ var command = process.argv[2];
 var term = process.argv.slice(3).join(" ");
 
 /* Function makes api request to bands in town website to find artist concerts */
-function findConcerts(artist) {
-    axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
+function findConcerts(artistName) {
+    axios.get("https://rest.bandsintown.com/artists/" + artistName + "/events?app_id=codingbootcamp")
         .then(function (response) {
             if (response.data.length) {
                 response.data.forEach(function (event) {
@@ -67,6 +67,45 @@ function findSongInfo(songName) {
     }
 }
 
+/* Function makes api request to OMDB website to obtain movie information */
+function findMovies(movieName) {
+    // If no movie name is not provided, set movieName variable to "Mr. Nobody"
+    if (movieName == "") movieName = "Mr. Nobody";
+    axios.get("http://www.omdbapi.com/?t=" + movieName + "&apikey=trilogy")
+        .then(function (response) {
+            // Check if response return movie info
+            if (response.data.Response === "True") {
+                console.log("\nMovie Title: ", response.data.Title);
+                console.log("Year: ", response.data.Year);
+                console.log("IMDB Rating: ", response.data.Ratings[0].Value);
+                console.log("Rotten Tomatoes Rating: ", response.data.Ratings[1].Value);
+                console.log("Country: ", response.data.Country);
+                console.log("Language: ", response.data.Language);
+                console.log("Movie Plot: ", response.data.Plot);
+                console.log("Actors: ", response.data.Actors);
+                console.log("\n▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬");
+            } else {
+                console.log(response.data.Error);
+            }
+        }).catch(function (error) {
+            if (error.response) {
+                // The request was made and the server responded with a status code
+                // that falls out of the range of 2xx
+                console.log(error.response.data);
+                console.log(error.response.status);
+                console.log(error.response.headers);
+            } else if (error.request) {
+                // The request was made but no response was received
+                // `error.request` is an object that comes back with details pertaining to the error that occurred.
+                console.log(error.request);
+            } else {
+                // Something happened in setting up the request that triggered an Error
+                console.log("Error", error.message);
+            }
+            console.log(error.config);
+        });
+}
+
 /* Evaluate the command variable to execute the statement associated each case value */
 switch (command) {
     case "concert-this":
@@ -75,7 +114,11 @@ switch (command) {
     case "spotify-this-song":
         findSongInfo(term);
         break;
+    case "movie-this":
+        findMovies(term);
+        break;
 }
+
 
 
 
