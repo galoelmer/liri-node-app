02c99d6dd24a6bc284937367391ef4c8cf2365a2
Elmer C. Galo <elmer.galo@outlook.com>
2019-11-13 15:12:55 -0500

    Add switch statement to evaluate app action and wrap api call to bands in tonw inside a function

--- liri.js
+++ liri.js
@@ -1,42 +1,56 @@
 require("dotenv").config();
 
-var keys =      require("./keys.js");
-var Spotify =   require ('node-spotify-api');
-var spotify =   new Spotify(keys.spotify);
-var moment =    require('moment');
-var axios =     require("axios");
-var fs =        require("fs");
+var keys = require("./keys.js");
+var Spotify = require('node-spotify-api');
+var spotify = new Spotify(keys.spotify);
+var moment = require('moment');
+var axios = require("axios");
+var fs = require("fs");
 
-var artist = process.argv.slice(2).join(" ");
+var command = process.argv[2];
+var term = process.argv.slice(3).join(" ");
 
-axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
-    .then(function (response) {
-        response.data.forEach(function(event){
-            // console.log(event);
-            console.log("\nVenue's Name: ", event.venue.name);
-            console.log("Venue's Location: ", event.venue.city + ", " + (event.venue.region || event.venue.country));
-            var date = moment(event.datetime);
-            console.log("Event's Date: ", date.format("MM/DD/YYYY"));
-            console.log("\n▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬");
+/* Function makes api request to bands in town website to find artist concerts */
+function findConcerts(artist) {
+    axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
+        .then(function (response) {
+            if (response.data.length) {
+                response.data.forEach(function (event) {
+                    console.log("\nVenue's Name: ", event.venue.name);
+                    console.log("Venue's Location: ", event.venue.city + ", " + (event.venue.region || event.venue.country));
+                    var date = moment(event.datetime);
+                    console.log("Event's Date: ", date.format("MM/DD/YYYY"));
+                    console.log("\n▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬");
+                });
+            } else {
+                console.log("No Upcoming Events");
+            }
+        })
+        .catch(function (error) {
+            if (error.response) {
+                // The request was made and the server responded with a status code
+                // that falls out of the range of 2xx
+                console.log(error.response.data);
+                console.log(error.response.status);
+                console.log(error.response.headers);
+            } else if (error.request) {
+                // The request was made but no response was received
+                // `error.request` is an object that comes back with details pertaining to the error that occurred.
+                console.log(error.request);
+            } else {
+                // Something happened in setting up the request that triggered an Error
+                console.log("Error", error.message);
+            }
+            console.log(error.config);
         });
-    })
-    .catch(function (error) {
-        if (error.response) {
-            // The request was made and the server responded with a status code
-            // that falls out of the range of 2xx
-            console.log(error.response.data);
-            console.log(error.response.status);
-            console.log(error.response.headers);
-        } else if (error.request) {
-            // The request was made but no response was received
-            // `error.request` is an object that comes back with details pertaining to the error that occurred.
-            console.log(error.request);
-        } else {
-            // Something happened in setting up the request that triggered an Error
-            console.log("Error", error.message);
-        }
-        console.log(error.config);
-    });
+}
+
+/* Evaluate the command variable to execute the statement associated each case value */
+switch (command) {
+    case "concert-this":
+        findConcerts(term);
+        break;
+}
 
 
 
