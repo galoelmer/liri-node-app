c85e6b87bf03f1c7bb20c2ca08e9065b4803eec0
Elmer C. Galo <elmer.galo@outlook.com>
2019-11-13 14:16:01 -0500

    Install and Declare variables for all require packages: axios, dotenv, momentJS, node-spotify-api

--- liri.js
+++ liri.js
@@ -1,1 +1,12 @@
 require("dotenv").config();
+
+var keys =      require("./keys.js");
+var Spotify =   require ('node-spotify-api');
+var spotify =   new Spotify(keys.spotify);
+var moment =    require('moment');
+var axios =     require("axios");
+var fs =        require("fs");
+
+
+
+
